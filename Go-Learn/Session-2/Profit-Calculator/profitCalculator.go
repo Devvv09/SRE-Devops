a19b5ea8c5e7447f18fa31e3ad001bb3490fd3cc
Devvrat Parashar <devparashar470@gmail.com>
2024-11-23 17:05:38 +0530

    Implement logic for finding profit

--- Go-Learn/Session-2/Profit-Calculator/profitCalculator.go
+++ Go-Learn/Session-2/Profit-Calculator/profitCalculator.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println()
+
+	var taxRate,revenue float64
+	var expenses int
+
+	fmt.Print("Please enter your taxRate, revenue, expenses:")
+	fmt.Scan(&taxRate,&revenue,&expenses)
+
+	fmt.Println("Your profit would be:",findProfit(taxRate,revenue,expenses))
+
+}
+
+func findProfit(taxRate,revenue float64,expenses int) float64 {
+
+	EBT := revenue - float64(expenses)
+	return EBT * (1 - taxRate / 100)
+	
 }
