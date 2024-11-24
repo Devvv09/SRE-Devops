17dfada25f1794662266926a92c07a378c3a72be
Devvrat Parashar <devparashar470@gmail.com>
2024-11-24 18:44:25 +0530

    Add Validation
    validation will check for the values which are negative or zero

--- Go-Learn/Session-2/Profit-Calculator/profitCalculator.go
+++ Go-Learn/Session-2/Profit-Calculator/profitCalculator.go
@@ -1,22 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func main(){
+func main() {
 
-	var taxRate,revenue float64
+	var taxRate, revenue float64
 	var expenses int
 
 	fmt.Print("Please enter your taxRate, revenue, expenses:")
-	fmt.Scan(&taxRate,&revenue,&expenses)
+	fmt.Scan(&taxRate, &revenue, &expenses)
 
-	fmt.Println("Your profit would be:",findProfit(taxRate,revenue,expenses))
+	profit, err := findProfit(taxRate, revenue, expenses)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(profit)
+	}
 
 }
 
-func findProfit(taxRate,revenue float64,expenses int) float64 {
+func findProfit(taxRate, revenue float64, expenses int) (float64, error) {
+	if taxRate < 0 || revenue < 0 || expenses < 0 {
+		return 0.0, errors.New("Error: values cannot be less than zero")
+	} else if taxRate == 0 || revenue == 0 || expenses == 0 {
+		return 0.0, errors.New("Error: values cannot be zero")
+	}
 
 	EBT := revenue - float64(expenses)
-	return EBT * (1 - taxRate / 100)
-	
+	return EBT * (1 - taxRate/100), nil
+
 }
