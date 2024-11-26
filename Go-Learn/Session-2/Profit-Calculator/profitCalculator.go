7507a67c845a5b80484ea5810567cddb10578d24
Devvrat Parashar <devparashar470@gmail.com>
2024-11-26 22:42:55 +0530

    Change data-type of findProfit() argument expences
    - change the data type of findProfit function argument expences from int to float
    
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- Go-Learn/Session-2/Profit-Calculator/profitCalculator.go
+++ Go-Learn/Session-2/Profit-Calculator/profitCalculator.go
@@ -10,8 +10,7 @@ const fileName = "output.txt"
 
 func main() {
 
-	var taxRate, revenue float64
-	var expenses int
+	var taxRate, revenue, expenses float64
 
 	fmt.Print("Please enter your tax rate, revenue, expenses:")
 	fmt.Scan(&taxRate, &revenue, &expenses)
@@ -19,53 +18,48 @@ func main() {
 	profit, err := findProfit(taxRate, revenue, expenses)
 
 	if err != nil {
+		panic(fmt.Sprintf("%v", err))
+
+	}
+	strProfit := fmt.Sprintf("Profit is %v\n", profit)
+	byteData := []byte(strProfit)
+	data, err := reader(fileName)
+
+	if err != nil {
 		fmt.Println(err)
-		panic(1)
+	}
 
-	} else {
+	data = append(data, byteData...)
+	err = writer(fileName, []byte(data))
+	if err != nil {
+		fmt.Println("Error:", err)
 
-		strProfit := fmt.Sprintf("Profit is %v\n",profit)
-		byteData := []byte(strProfit)
-		data, err := reader(fileName)
-
-		if err != nil{
-			fmt.Println(err)
-		}
-		
-		data = append(data, byteData...)
-
-		err = writer(fileName,[]byte(data))
-		if err != nil{
-			fmt.Println("Error:",err)
-		}
 	}
 
 }
 
-func findProfit(taxRate, revenue float64, expenses int) (float64, error) {
+func findProfit(taxRate, revenue , expenses float64) (float64, error) {
 
-	if taxRate < 0 || revenue < 0 || expenses < 0 {
+	if taxRate <= 0 || revenue <= 0 || expenses <= 0 {
 		return 0.0, errors.New("Error: values cannot be less than zero")
-	} else if taxRate == 0 || revenue == 0 || expenses == 0 {
-		return 0.0, errors.New("Error: values cannot be zero")
-	}
+	} 
 
-	EBT := revenue - float64(expenses)
-	return EBT * (1 - taxRate/100), nil
+	ebt := revenue - expenses
+	return ebt * (1 - taxRate/100), nil
 
 }
 
 func writer(fileName string, data []byte) error {
 
-	err := os.WriteFile(fileName,data,0700)
-	if err != nil{
-		return fmt.Errorf("Erorr: %v",err)
+	err := os.WriteFile(fileName, data, 0700)
+	if err != nil {
+		return fmt.Errorf("Erorr: %v", err)
 	}
 	return nil
 }
 
-func reader(fileName string)([]byte, error){
-	
+func reader(fileName string) ([]byte, error) {
+
 	data, err := os.ReadFile(fileName)
 	if err != nil {
 		return nil, errors.New(err.Error())
