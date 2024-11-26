98056a2e811d1eda839613caba8356482ce10e91
Devvrat Parashar <devparashar470@gmail.com>
2024-11-26 22:16:02 +0530

    Add multi-line print statement
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- Go-Learn/Session-2/Calculator/calculator.go
+++ Go-Learn/Session-2/Calculator/calculator.go
@@ -7,56 +7,58 @@ import (
 
 func main(){
 	
-	var choice,num1,num2 int
+	var choice,num1,num2 float64
 
-	fmt.Println("1 for addition")
-	fmt.Println("2 for substraction")
-	fmt.Println("3 for multiplication")
-	fmt.Println("4 for division")
-	fmt.Println("5 for power")
-
-	fmt.Println("Enter your choice!")
+	fmt.Println(`
+		1 for addition
+		2 for substraction
+		3 for multiplication
+		4 for division
+		5 for power
+		0 for exit
+	
+		What is your choice?: 
+	`)
 	fmt.Scan(&choice)
 
 	fmt.Println("Enter number 1 and number 2")
-	fmt.Scan(&num1,&num2)
+	fmt.Scan(&num1, &num2)
 
 	switch choice{
 		case 1:
 			fmt.Println("Result:",add(num1,num2))
-			break
 		case 2:
 			fmt.Println("Result is:",substract(num1,num2))
-			break
+			
 		case 3:
-			fmt.Println("Result:",multiplication(num1,num2))
-			break
+			fmt.Println("Result:",multiply(num1,num2))
+			
 		case 4:
 			fmt.Println("Result is:",division(num1,num2))
-			break
+			
 		case 5:
 			fmt.Println("Result is",pow(num1,num2))
-			break
+			
 	}
 }
 
-func add(num1,num2 int) int {
+func add(num1,num2 float64) float64 {
 	return num1 + num2
 }
 
-func substract(num1,num2 int) int {
+func substract(num1,num2 float64) float64 {
 	return num1 - num2
 }
 
-func multiplication(num1,num2 int) int {
+func multiply(num1,num2 float64) float64 {
 	return num1 * num2
 }
 
-func division(num1,num2 int) float32 {
-	return float32(num1 / num2)
+func division(num1,num2 float64) float64 {
+	return float64(num1 / num2)
 }
 
-func pow(num1,num2 int) int {
-	return int(math.Pow(float64(num1),float64(num2)))
+func pow(num1,num2 float64) float64 {
+	return (math.Pow((num1),(num2)))
 }
 
