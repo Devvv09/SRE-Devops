14941371957a5ef1e5a2af01ccc83176160c340e
Devvrat Parashar <devparashar470@gmail.com>
2025-01-25 19:14:51 +0530

    Delete env file

--- Go-Learn/Student-mgmt-api/config/config.go
+++ Go-Learn/Student-mgmt-api/config/config.go
@@ -3,7 +3,6 @@ package config
 import (
 	"database/sql"
 	"fmt"
-
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
@@ -11,12 +10,12 @@ import (
 var DB *sql.DB
 var err error
 
-func ConnectDB() (error) {
+func ConnectDB() error {
 	
-	// fmt.Println(viper.GetInt("DB_PORT"))
 	LoadEnviromentVariable()
-	connStr := fmt.Sprintf("host=%s port=%v user=%s password=%s dbname=%s sslmode=disable",viper.GetString("DB_HOST"),viper.GetInt("DB_PORT"),viper.GetString("DB_USER"),viper.GetString("DB_PASSWORD"),viper.GetString("DB_NAME"))
-
+	// fmt.Println(viper.GetInt("DB_PORT"))
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",viper.GetString("DB_HOST"),viper.GetString("DB_PORT"),viper.GetString("DB_USER"),viper.GetString("DB_PASSWORD"),viper.GetString("DB_NAME"))
+	fmt.Println(connStr)
 	DB, err = sql.Open("postgres", connStr)
 	if err != nil {
 		return  err
