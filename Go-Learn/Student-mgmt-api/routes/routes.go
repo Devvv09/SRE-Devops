af3a81a14f17a654aa61c947c616f3c7ce851d86
Devvrat Parashar <devparashar470@gmail.com>
2025-01-25 19:32:18 +0530

    Implement grouping for apis

--- Go-Learn/Student-mgmt-api/routes/routes.go
+++ Go-Learn/Student-mgmt-api/routes/routes.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"students-mgmt-api/controllers"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,16 +9,13 @@ func Routes() *gin.Engine {
 
 	r := gin.Default()
 
-	r.GET("/api/v1/students", controllers.GetAllStudents)
-
-	r.GET("/api/v1/students/:id", controllers.GetStudent)
-
-	r.POST("/api/v1/students", controllers.AddStudent )
-
-	r.PUT("/api/v1/students/:id",controllers.UpdateStudent )
-
-	r.DELETE("/api/v1/students/:id",controllers.DeleteStudent)
-
+	apiGroup := r.Group("/api/v1")
+	{
+		apiGroup.GET("/students", controllers.GetAllStudents)
+		apiGroup.GET("/students/:id", controllers.GetStudent)
+		apiGroup.POST("/students", controllers.AddStudent)
+		apiGroup.PUT("/students/:id", controllers.UpdateStudent)
+		apiGroup.DELETE("/students/:id", controllers.DeleteStudent)
+	}
 	return r
-
 }
